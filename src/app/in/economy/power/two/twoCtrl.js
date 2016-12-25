fc00a125475c6d20292d21b2f536da195e29339d
Sam <1541491@tongji.edu.cn>
2016-12-25 21:05:15 +0800

    第二图

--- src/app/in/economy/power/two/twoCtrl.js
+++ src/app/in/economy/power/two/twoCtrl.js
@@ -1,104 +1,169 @@
 export default ($scope, $rootScope, qService, economyPowerRes) => {
 	'ngInject';
 
-	const getChart = (yearArr, powerArr, gdpArr) => {
-		$scope.TotalPowerGDPChart={
-			credits:{
-				enabled:false // 禁用版权信息
-			},
-			options:{
-				chart: {
-					type: 'spline',
-				},
-				exporting: {
-					enabled: false, // 取消打印menu
-				},
-			},
-			title: {
-				text: ''
-			},
-			subtitle: {
-				text: '',
-				x: -20
-			},
-			xAxis: {
-				categories: yearArr,
-				plotBands: [{
-					from: 9.5,
-					to: 12.5,
-					label: {
-						text:"预测区",
-						style: {
-							color: 'grey',
-						},
-					},
-					color: 'rgba(68, 170, 213, .1)'
-				}],
-			},
-
-			plotOptions: {
-				series: {
-					allowPointSelect: true,
-					point: {
-						events: {
-							click: function () {
-								alert('Category: ');
-							}
-						}
+	const getIndustryData = function(year) {
+		let getIndustryParams = {
+	    	'year': year, 
+	    };
+		let getTotalPromise = qService.httpGetWithToken(economyPowerRes.getIndustry, getIndustryParams, {});
+		getTotalPromise.then(function(data) {
+			$scope.totalData = data.data;
+			console.log($scope.totalData);
+			$scope.VocationChartPiePower = {
+		    	credits:{
+		    	    enabled:false // 禁用版权信息
+		    	},
+		    	options:{
+					exporting: {
+						enabled: false, // 取消打印menu
 					}
-				}
-			},
-			yAxis: {
-				title: {
-					text: '增长率 (%)'
 				},
-				plotLines: [{
-					value: 0,
-					width: 1,
-					color: '#808080'
-				}]
-			},
-			tooltip: {
-				valueSuffix: '%',
-			},
-			legend: {
-				layout: 'vertical',
-				align: 'right',
-				verticalAlign: 'middle',
-				borderWidth: 0
-			},
-			series: [{
-				name: '工业用电量同比增长率',
-				data: powerArr,
-				color:'#23B7E5',
-				}, {
-				name: 'GDP同比增长率',
-				data: gdpArr,
-				color:'#DAA520',
-			}]
-		};
+		    	chart: {
+		    	    plotBackgroundColor: null,
+		    	    plotBorderWidth: null,
+		    	    plotShadow: false
+		    	},
+		    	title: {
+		    	    text: '行业用电量增速对比',
+		    	    style: {
+		    	    	fontSize: "15px",
+		    	    }
+		    	},
+		    	subtitle: {
+		    	    text: getIndustryParams.year+'年度',
+		    	},
+		    	tooltip: {
+		    	    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+		    	},
+		    	xAxis: {
+		    	    categories:[
+		    	    '农、林、牧、渔业',
+		    	    '工业',
+		    	    '建筑业',
+		    	    '交通运输、仓储和邮政业',
+		    	    '信息传输、计算机服务和软件业',
+		    	    '批发和零售、住宿和餐饮业',
+		    	    '金融、房地产、商务及居民服务业',
+		    	    '公共事业及管理组织'],
+
+		    	    labels: {
+		    	        rotation: -45,
+		    	        align: 'right',
+		    	        style: {
+		    	            fontSize: '8px',
+		    	            fontFamily: 'Verdana, sans-serif'
+		    	        }
+		    	    }
+		    	},
+		    	yAxis: {
+		    	    title: {
+		    	        text: '增长率 (%)'
+		    	    },
+		    	    plotLines: [{
+		    	        value: 0,
+		    	        width: 1,
+		    	        color: '#808080'
+		    	    }]
+		    	},
+		    	plotOptions: {
+		    	    pie: {
+		    	        allowPointSelect: true,
+		    	        cursor: 'pointer',
+		    	        dataLabels: {
+		    	            enabled: true,
+		    	            color: '#000000',
+		    	            connectorColor: '#000000',
+		    	            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+		    	        }
+		    	    }
+		    	},
+		    	series: [{
+		    	    type: 'column',
+		    	    name: '同比增长率',
+		    	    data: $scope.totalData.powers,
+		    	    color:'#23B7E5',
+		    	}]
+		    };
+		    $scope.VocationChartPieGdp = {
+		    	credits:{
+		    	    enabled:false // 禁用版权信息
+		    	},
+		    	options:{
+					exporting: {
+						enabled: false, // 取消打印menu
+					}
+				},
+		    	chart: {
+		    	    plotBackgroundColor: null,
+		    	    plotBorderWidth: null,
+		    	    plotShadow: false
+		    	},
+		    	title: {
+		    	    text: '行业产值增速对比',
+		    	    style: {
+		    	    	fontSize: "15px",
+		    	    }
+		    	},
+		    	subtitle: {
+		    	    text: getIndustryParams.year+'年度',
+		    	},
+		    	tooltip: {
+		    	    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+		    	},
+		    	plotOptions: {
+		    	    pie: {
+		    	        allowPointSelect: true,
+		    	        cursor: 'pointer',
+		    	        dataLabels: {
+		    	            enabled: true,
+		    	            color: '#000000',
+		    	            connectorColor: '#000000',
+		    	            format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+		    	        }
+		    	    }
+		    	},
+		    	xAxis: {
+		    	    categories:[
+		    	    '农、林、牧、渔业',
+		    	    '工业',
+		    	    '建筑业',
+		    	    '交通运输、仓储和邮政业',
+		    	    '信息传输、计算机服务和软件业',
+		    	    '批发和零售、住宿和餐饮业',
+		    	    '金融、房地产、商务及居民服务业',
+		    	    '公共事业及管理组织'],
+		    	    labels: {
+		    	        rotation: -45,
+		    	        align: 'right',
+		    	        style: {
+		    	            fontSize: '8px',
+		    	            fontFamily: 'Verdana, sans-serif'
+		    	        }
+		    	    }
+		    	},
+		    	yAxis: {
+		    	    title: {
+		    	        text: '增长率 (%)'
+		    	    },
+		    	    plotLines: [{
+		    	        value: 0,
+		    	        width: 1,
+		    	        color: '#808080'
+		    	    }]
+		    	},
+		    	series: [{
+		    	    type: 'column',
+		    	    name: '同比增长率',
+		    	    data: $scope.totalData.gdps,
+		    	    color:'#DAA520',
+		    	}]
+		    };
+		},function(error) {});
+    };
+    $scope.currentYear = "2015";
+    getIndustryData(2015);
+	$scope.changeYear = function(param){
+		getIndustryData(parseInt(param));
+		$scope.currentYear = param;
 	};
-    $rootScope.loading = true;
-    qService.httpGetWithToken(economyPowerRes.getTotal, {}, {}).then((data) => {
-        if (data.code == "200") {
-        	$scope.getData = data.data;
-			$scope.oneword = '用电量增减变动趋势与GDP增减变动趋势基本一致，GDP增长时，用电量也增长，增速相似；GDP回落时，用电量增长也回落;预测阶段用电量增速将大于GDP增速。';
-			$scope.yearvalue = $scope.getData.years;
-			$scope.gdprate = $scope.getData.gdps;
-			$scope.powerrate = $scope.getData.powers;
-			    
-			getChart($scope.yearvalue, $scope.powerrate, $scope.gdprate); // 默认显示历史数据
-			$scope.changeTotalChart = function(param){
-				$scope.TotalPowerGDPChart.options.chart.type = param;
-			};
-			$scope.showTotalTable = function(){
-				$scope.totalshow= !$scope.totalshow;
-			};
-			
-        }
-    }, (err) => {
-        console.log(err);
-    }).finally(() => {
-        $rootScope.loading = false;
-    });
 };
