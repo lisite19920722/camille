836057201d0150160678097fb1b22e8a4a269e17
winter <1944844113@qq.com>
2016-12-27 18:09:04 +0800

    饼图切换数据成功！

--- src/app/in/population/labor/three/threeCtrl.js
+++ src/app/in/population/labor/three/threeCtrl.js
@@ -12,36 +12,6 @@ export default ($scope, $stateParams, qService, populationRes) => {
 	qService.httpGetWithToken(populationRes.getLaborGdpRelationPreData,{},{})
 	.then((resource) => {
 
-		//切换数据
-		var firstFive =(year) =>{
-			console.log('firstFive');
-            // $scope.populationPie = comChart(dataP.slice(0,10), relate1, dataG.slice(0,10), yearLen1); 
-        };
-
-        $scope.toggleData1 = (year) => {
-        	switch(year){
-        		case $scope.yearSelect1[0]:
-        		firstFive(year);
-        		break;
-        		case $scope.yearSelect1[1]:
-        		secondFive(year);
-        		break;
-        		case $scope.yearSelect1[2]:
-        		thirdFive(year);
-        		break;
-        		case $scope.yearSelect1[3]:
-        		forthFive(year);
-        		break;
-        		case $scope.yearSelect1[4]:
-        		fifthFive(year);
-        		break;
-        		case $scope.yearSelect1[5]:
-        		sixthFive(year);
-        		break;
-        	};           
-
-        };
-
 		//第一产业数据
 		var dataI1 = new Array(30);
 		var iRate1 = new Array(30);
@@ -60,12 +30,12 @@ export default ($scope, $stateParams, qService, populationRes) => {
 
         var sumP = new Array(30);
         var sumI = new Array(30);
-
+        //取数据，5年取一次
         for(let i=0; i<30; i=i+5) {
 
-           dataP1[i] = resource.data[i].preFirstLaborPopulation;//第一产业人口
-           dataP2[i] = resource.data[i].preSecondLaborPopulation;//第二产业人口
-           dataP3[i] = resource.data[i].preThirdLaborPopulation;//第三产业人口
+           dataP1[i/5] = resource.data[i].preFirstLaborPopulation;//第一产业人口
+           dataP2[i/5] = resource.data[i].preSecondLaborPopulation;//第二产业人口
+           dataP3[i/5] = resource.data[i].preThirdLaborPopulation;//第三产业人口
        }
 		   // sumP[i] = resource.data[i].preLaborPopulation;//总人口
      //       pRate1[i] = dataP1[i]/sumP[i];//第一产业人口比例
@@ -80,8 +50,131 @@ export default ($scope, $stateParams, qService, populationRes) => {
      //       iRate1[i] = dataI1[i]/sumI[i];//第一产业产值比例
      //       iRate2[i] = dataI2[i]/sumI[i];//第二产业产值比例
      //       iRate3[i] = dataI3[i]/sumI[i];//第三产业产值比例
+     
+       var comChart = (data1, data2, data3) => {
+         return {
+       	options: {
+       		 exporting: {
+                enabled: false, // 取消打印menu
+            },
+       		colors:['#7CB5EC','#929dce','#90ED7D'],
+       		chart: {
+       			plotBackgroundColor: null,
+       			plotBorderWidth: null,
+       			plotShadow: false,
+       			margin: [30, 30, 30, 30],
+       		},
+       		credits:{
+       			enabled:false
+       		},
+       		title: {
+       			text: "太仓市2020年产业就业人口比重分析预测"
+       		},
+       		tooltip: {
+       			enabled: true,
+       			pointFormat: ' 数量:{point.y:1.f}(万人)</b> '
+       		},
+       		   plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        zIndex: 40,
+                        enabled: true,
+                        format: '<b>{point.name}</b>: <br><center>{point.percentage:.1f} %</center>',
+                        style: {
+                            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                        }
+                    },
+                    showInLegend: true
+                }
+            },
+       		legend:{
+       			itemStyle:{
+       				fontWeight:'normal'
+       			}
+       		}
+       	},
+       	series: [{
+       		type: 'pie',
+       		name: '',
+       		data:[
+       		['第一产业人口',data1/10000],
+       		['第二产业人口',data2/10000],
+       		['第三产业人口',data3/10000]
+       		]
+       	}]
 
-       $scope.populationPie ={
+       };
+
+       $scope.industryPie = {
+       	chart: {
+       		plotBackgroundColor: null,
+       		plotBorderWidth: null,
+       		plotShadow: false
+       	},
+       	title: {
+       		text: 'Browser market shares at a specific website, 2014'
+       	},
+       	tooltip: {
+       		pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+       	},
+       	plotOptions: {
+       		pie: {
+       			allowPointSelect: true,
+       			cursor: 'pointer',
+       			dataLabels: {
+       				enabled: false
+       			},
+       			showInLegend: true
+       		}
+       	},
+       	series: [{
+       		type: 'pie',
+       		name: 'Browser share',
+       		data: [
+       		['Firefox',   45.0],
+       		['IE',       26.8],
+       		{
+       			name: 'Chrome',
+       			y: 12.8,
+       			sliced: true,
+       			selected: true
+       		},
+       		['Safari',    8.5],
+       		['Opera',     6.2],
+       		['Others',   0.7]
+       		]
+       	}]
+       }
+       }
+       
+        $scope.toggleData1 = (year) => {
+        	switch(year){
+        		case $scope.yearSelect1[0]:
+                $scope.populationPie = comChart(dataP1[0], dataP2[0], dataP3[0]); 
+        		break;
+        		case $scope.yearSelect1[1]:
+        		$scope.populationPie = comChart(dataP1[1], dataP2[1], dataP3[1]); 
+        		console.log('执行到第二个了');
+        		break;
+        		case $scope.yearSelect1[2]:
+        		thirdFive(year);
+        		break;
+        		case $scope.yearSelect1[3]:
+        		forthFive(year);
+        		break;
+        		case $scope.yearSelect1[4]:
+        		fifthFive(year);
+        		break;
+        		case $scope.yearSelect1[5]:
+        		sixthFive(year);
+        		break;
+        	};           
+
+        };
+
+       $scope.populationPie = {
        	options: {
        		 exporting: {
                 enabled: false, // 取消打印menu
