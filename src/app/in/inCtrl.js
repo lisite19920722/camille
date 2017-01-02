0f85264fff676cb0db7a4f724b851e48b667c9c4
Sam <1541491@tongji.edu.cn>
2017-01-02 22:12:32 +0800

    权限控制放到前端

--- src/app/in/inCtrl.js
+++ src/app/in/inCtrl.js
@@ -1,4 +1,4 @@
-export default(AuthTool, $state) => {
+export default($rootScope, AuthTool, $state, qService) => {
 	'ngInject';
 
 	const JQueryDOMOperation = () => {
@@ -27,4 +27,65 @@ export default(AuthTool, $state) => {
 
 		// 导航中对应当前页面的标签激活 最后补充
 	}();
+
+	// 权限控制放在前端
+	
+	// 获得账户信息
+	try {
+		let mobile = AuthTool.getLoginInfo().mobile;
+		switch(mobile) {
+			case "system":
+			case "sz01":
+			case "jxw01": 
+				break; // 全部显示
+			case "tjj01":
+			case "czj01": {
+				$("#environmentA").hide(0);
+				$("#populationA").hide(0);
+				break;
+			}
+			case "gaj01":
+			case "xfdd01":
+			case "sjjdd01":
+			case "xfj01":
+			case "ajj01":
+			case "gtj01":
+			case "wjj01":
+			case "jtj01":
+			case "cgj01":
+			case "wsj01": { // 全部隐藏 没有用到这些局的数据 不提供BI服务
+				$("#environmentA").hide(0);
+				$("#populationA").hide(0);
+				$("#economyA").hide(0);
+				break;
+			}
+			case "hbj01": {
+				$("#populationA").hide(0);
+				$("#economyA").hide(0);
+				break;
+			}
+			case "slj01": {
+				$("#populationA").hide(0);
+				$("#economyA").hide(0);
+
+				$("#environmentAir").hide(0);
+				break;
+			}
+			case "jsw01": {
+				$("#environmentA").hide(0);
+				$("#economyA").hide(0);
+				break;
+			}
+			case "rsj01":
+			case "jyj01": {
+				$("#environmentA").hide(0);
+				$("#economyA").hide(0);
+				$("#populationStructure").hide(0);
+				$("#populationLabor").hide(0);
+				break;
+			}
+		}
+	} catch(e) {
+		console.log(e);
+	}
 };
